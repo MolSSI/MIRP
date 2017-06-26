b9811d54a840fc6d6cf371e0038b14a665f63231
Benjamin Pritchard <ben@bennyp.org>
2017-06-26 17:32:28 -0400

    Add interval boys function to run_boys_test

--- mirp_bin/run_boys_test.cpp
+++ mirp_bin/run_boys_test.cpp
@@ -46,6 +46,51 @@ static long run_boys_test_mp(const boys_data & data, long target_prec, long work
     return nfailed;
 }
 
+static long run_boys_test_interval(const boys_data & data, long target_prec, long working_prec)
+{
+    int maxm = 0;
+    for(auto & it : data.values)
+        maxm = std::max(maxm, it.m);
+
+    arb_t t_mp, vref_mp;
+    arb_init(t_mp);
+    arb_init(vref_mp);
+
+    auto F_mp = std::unique_ptr<arb_t[]>(new arb_t[maxm+1]);
+    for(int i = 0; i <= maxm; i++)
+        arb_init(F_mp[i]);
+
+    long nfailed = 0;
+
+    for(const auto & it : data.values)
+    {
+        arb_set_str(t_mp, it.t.c_str(), working_prec);
+        arb_set_str(vref_mp, it.value.c_str(), target_prec);
+        mirp_boys_interval(F_mp.get(), it.m, t_mp, working_prec);
+
+        arb_set_round(F_mp[it.m], F_mp[it.m], target_prec);
+
+        if(!arb_overlaps(F_mp[it.m], vref_mp))
+        {
+            std::cout << "Entry failed test: m = " << it.m << " t = " << it.t << "\n";
+            char * s1 = arb_get_str(F_mp[it.m], 1000, 0);
+            char * s2 = arb_get_str(vref_mp, 1000, 0);
+            std::cout << "   Calculated: " << s1 << "\n";
+            std::cout << "    Reference: " << s2 << "\n";
+            free(s1);
+            free(s2);
+            nfailed++;
+        }
+    }
+
+    arb_clear(t_mp);
+    arb_clear(vref_mp);
+    for(int i = 0; i <= maxm; i++)
+        arb_clear(F_mp[i]);
+
+    return nfailed;
+}
+
 static long run_boys_test_double(const boys_data & data)
 {
     int maxm = 0;
@@ -94,6 +139,8 @@ long run_boys_test(const std::string & filename, const std::string & floattype, 
 
     if(floattype == "mp")
         nfailed = run_boys_test_mp(data, target_prec, working_prec);
+    else if(floattype == "interval")
+        nfailed = run_boys_test_interval(data, target_prec, working_prec);
     else if(floattype == "double")
         nfailed = run_boys_test_double(data);
     else
