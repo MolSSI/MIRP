21f1fd63f1788fee084d12bac3b241d2161a4df6
Benjamin Pritchard <ben@bennyp.org>
2017-08-27 11:48:14 -0400

    Fix cmake issue with hard-coded paths

--- cmake/mirpDependencies.cmake
+++ cmake/mirpDependencies.cmake
@@ -10,11 +10,18 @@ macro(mirp_find_library libname)
     get_filename_component(MIRP_${libname}_DIR ${MIRP_${libname}_DIR} DIRECTORY)
     message(STATUS "Found ${libname} library: ${MIRP_${libname}_LIBRARY}")
     message(STATUS "Found ${libname} path: ${MIRP_${libname}_DIR}")
-    list(APPEND MIRP_DEPS_LIBRARIES "${MIRP_${libname}_LIBRARY}")
-    list(APPEND MIRP_DEPS_INCLUDE_DIRS "${MIRP_${libname}_DIR}/include")
+
+    add_library(mirp_imported_${libname} SHARED IMPORTED GLOBAL)
+    set_target_properties(mirp_imported_${libname} PROPERTIES IMPORTED_LOCATION ${MIRP_${libname}_LIBRARY})
+    set_target_properties(mirp_imported_${libname} PROPERTIES INTERFACE_LINK_LIBRARIES ${MIRP_${libname}_LIBRARY})
+    set_target_properties(mirp_imported_${libname} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${MIRP_${libname}_DIR}/include)
+    list(APPEND MIRP_DEPS_TARGETS "mirp_imported_${libname}")
+
 endmacro()
 
 mirp_find_library(arb)
 mirp_find_library(flint)
 mirp_find_library(mpfr)
 mirp_find_library(gmp)
+
+
